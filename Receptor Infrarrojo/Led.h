0b7faf2fdf822e1d1119ef741b6fb87671f5e9b0
GabrielGomezGG <gabrielgomezgg1997@gmail.com>
2021-12-01 17:50:04 -0300

    se agrego Led.h

---
+++ Receptor Infrarrojo/Led.h
@@ -0,0 +1,85 @@
+
+
+class Led
+{
+private:
+    float subir, encenderApagar, bajar;   //codigos de los botones en decimal 
+    bool isEncendido;   //verifica si el led esta encendido o apagado
+    int numeroDePin;    //numero de pin de arduino 
+    int brillo;     //de 0 a 255
+    String color;
+
+public:
+
+    //pin, boton subir , boton encendido de led  , boton bajar , color
+    Led(int _numeroDePin, float _bajar, float _encenderApagar, float _subir,  String _color){
+        numeroDePin = _numeroDePin;
+        subir = _subir;
+        encenderApagar = _encenderApagar;
+        bajar = _bajar;
+        isEncendido = false;
+        brillo = 0; 
+        color = _color;
+    } 
+
+    //encender o apagar el led
+    //apaga (0) o enciende el led (255)
+    void offOn(){
+        if(!isEncendido){
+            isEncendido = true;
+            brillo = 255;
+            analogWrite(numeroDePin, brillo);
+        }
+        else{
+            isEncendido = false;
+            brillo = 0;
+            analogWrite(numeroDePin, brillo);
+        }
+    }
+
+
+    //sube el brillo +15
+    void bajarBrillo(){
+        if(isEncendido){
+            if(brillo >= 0 && brillo < 255){
+                brillo+=15;
+            }
+        }
+    }
+
+    //baja el brillo -15
+    void subirBrillo(){
+        if(isEncendido){
+            if(brillo > 0 && brillo <= 255){
+                brillo-=15;
+            }
+        }
+    }
+
+    //getter
+
+    String getColor(){
+        return color;
+    }
+
+    int getNumeroDePin(){
+        return numeroDePin;
+    }
+
+    float getSubir(){
+        return subir;
+    }
+
+    float getBajar(){
+        return bajar;
+    }
+
+    float getEncenderApagar(){
+        return encenderApagar;
+    }
+
+    //cambia el pin actual por el ingresado por parametro.
+    void setPin(int newPin){
+        numeroDePin = newPin;
+    }
+};
---
+++ Receptor Infrarrojo/ReceptorInflarojoMejorado.ino
@@ -0,0 +1,84 @@
+#include <IRremote.h>
+#include "Led.h"
+
+// #define B0 0xFD30CF //Boton 0 
+// #define B1 0xFD08F7 //Boton 1 
+// #define B2 0xFD8877 //Boton 2
+// #define B3 0xFD48B7 //Boton 3
+// #define B4 0xFD28D7 //Boton 4
+// #define B5 0xFDA857 //Boton 5
+// #define B6 0xFDA857 //Boton 6
+// #define B7 0xFD08F7 //Boton 7
+// #define B8 0xFD8877 //Boton 8
+// #define B9 0xFD48B7 //Boton 9
+// #define B0 0xFD30CF //Boton 0
+// #define B100 0xFDB04F //Boton 100
+// #define B200 0xFD708F //Boton 200
+// #define BCHu 0xFD00FF //Boton off
+// #define BCH 0xFD807F  //Boton guardar
+// #define BCHd 0xFD40BF //Boton borrar
+
+const float b1 = 16582903;
+const float b2 = 16615543;
+const float b3 = 16599223;
+const float b4 = 16591063;
+const float b5 = 16623703;
+const float b6 = 16607383; 
+const float b7 = 16586983; 
+const float b8 = 16619623;
+const float b9 = 16603303;
+
+Led ledR,ledV,ledA;
+
+int SENSOR = 11;    //sensor inflarojo en el pin11
+IRrecv irrecv(SENSOR);  //objeto de la clase IRrecv (resive las señales del sensor)
+decode_results codigo;
+
+void setup()    
+{
+    ledR = Led(3,b1,b2,b3,"Rojo");
+    ledV = Led(6,b4,b5,b6,"Verde");
+    ledA = Led(5,b7,b8,b9,"Azul");
+    
+	Serial.begin(9600);
+    irrecv.enableIRIn();
+
+    //leds output
+    pinMode(ledR.getNumeroDePin(),OUTPUT);
+    pinMode(ledV.getNumeroDePin(),OUTPUT);
+    pinMode(ledA.getNumeroDePin(), OUTPUT);
+
+}
+
+void loop()
+{
+	if(irrecv.decode(&codigo)){
+        Serial.println(codigo.value,HEX);
+
+        accionLed(ledR);
+        accionLed(ledV);
+        accionLed(ledA);
+
+        irrecv.resume();
+    }
+    delay(100);
+}
+
+//dependiendo el boton que toques en el control, se hara la siguiente opcion:
+//-bajar el brillo del led
+//-apaga o enciden el led 
+//-sube el brillo del led 
+void accionLed(Led led){
+
+    if(codigo.value == led.getBajar()){
+        led.bajarBrillo();
+    }
+
+    if(codigo.value == led.getEncenderApagar()){
+        led.offOn();
+    }
+
+    if(codigo.value == led.getEncenderApagar()){
+        led.subirBrillo();
+    }
+}
